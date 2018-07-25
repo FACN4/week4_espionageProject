4d24a87fc832f5b90f3542ff466d66a4a259813c
tawfiknasser <tawfiknasser@gmail.com>
2018-07-25 14:53:24 +0300

    added 404 page & added img

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ const handlers = require("./handlers");
 
 var router = function(req, res) {
   var url = req.url;
+  console.log(url)
+
   if (url === "/") {
     // run index.html
     handlers.handlerHome(req, res);
@@ -9,8 +11,10 @@ var router = function(req, res) {
    else if (url.includes("/citysearch")) {
      handlers.handlerCities(req,res);
   }
-  else if (["/index.js", "/style.css","/dom.js","/xhr.js"].includes(url)) {
+  else if (["/index.js", "/style.css","/dom.js","/xhr.js","/imgs/404Ben.jpg"].includes(url)) {
     // run public files
+    console.log(url)
+
     handlers.handlerPublic(req,res,url);
   } else {
     //404
