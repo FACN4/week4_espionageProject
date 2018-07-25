587f5678c062612aacd0af030decc17b2cc72e2c
Ben Newman <benjamin-newman@live.co.uk>
2018-07-25 12:02:04 +0300

    dropdown menu created

--- public/dom.js
+++ public/dom.js
@@ -1,7 +1,52 @@
-var searchForm = document.getElementById('searchForm');
-var searchInput = document.getElementById('searchInput');
+var searchForm = document.getElementById("searchForm");
+var searchInput = document.getElementById("searchInput");
 console.log(searchInput);
 
-var sendForm = function () {
+var sendForm = function() {
   console.log(searchInput.value);
 };
+
+//creates a drop down menu from the array of cities. Is called by an XHR request.
+const arrOfCities = [
+  {
+    country: "GB",
+    name: "York",
+    lat: "53.95763",
+    lng: "-1.08271"
+  },
+  {
+    country: "GB",
+    name: "Yetminster",
+    lat: "50.89579",
+    lng: "-2.57959"
+  },
+  {
+    country: "GB",
+    name: "Yeovil",
+    lat: "50.94159",
+    lng: "-2.63211"
+  }
+];
+
+var appendCities = function(arrOfCities) {
+  let ul = document.getElementById("dropdown");
+  arrOfCities.forEach(function(city) {
+    let li = document.createElement("li");
+    let divCity = document.createElement("div");
+    let divCountry = document.createElement("div");
+    divCity.textContent = city.name;
+    divCountry.textContent = "(" + city.country + ")";
+    ul.appendChild(li);
+    li.appendChild(divCity);
+    li.appendChild(divCountry);
+  });
+};
+
+appendCities(arrOfCities);
+
+//create clickable list items. Ee
+// var clickableItem = function(){
+//   let cityForPixlr =
+// }
+//
+// li.addEventListener('click', clickableItem(), false )
