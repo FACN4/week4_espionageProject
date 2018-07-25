be6b5d437b0e6e1755c665a242bbfa5bf39b1ee8
Ben Newman <benjamin-newman@live.co.uk>
2018-07-25 14:52:56 +0300

    added clickable list

--- public/dom.js
+++ public/dom.js
@@ -1,10 +1,11 @@
-var searchInput = document.getElementById('searchInput');
+//searchInput from index.HTML
+var searchInput = document.getElementById("searchInput");
 console.log(searchInput);
 
-var sendForm = function () {
-  var url = searchWrapper(searchInput.value);
+var sendForm = function() {
+  var url = searchWrapper(searchInput.value); // searchWrapper defined in xhr.js
 
-  xhrAPI(url, appendCities);
+  xhrAPI(url, appendCities); //xhrAPI defined in xhr.js
 };
 
 //creates a drop down menu from the array of cities. Is called by an XHR request.
@@ -30,7 +31,8 @@ const arrOfCities = [
 ];
 
 var appendCities = function(arrOfCities) {
-  let ul = document.getElementById("dropdown");
+  var ul = document.getElementById("dropdown");
+  ul.innerHTML = "";
   arrOfCities.forEach(function(city) {
     let li = document.createElement("li");
     let divCity = document.createElement("div");
@@ -40,12 +42,15 @@ var appendCities = function(arrOfCities) {
     ul.appendChild(li);
     li.appendChild(divCity);
     li.appendChild(divCountry);
+    li.addEventListener(
+      "click",
+      function(e) {
+        var cityForAPI = e.target.firstChild.textContent;
+        var query = city.name;
+        var id = "pxlrimage";
+        pixabyXhrApi(query, id); //Needs consolel
+      },
+      false
+    );
   });
 };
-
-//create clickable list items. Ee
-// var clickableItem = function(){
-//   let cityForPixlr =
-// }
-//
-// li.addEventListener('click', clickableItem(), false )
