4955aaa0c036c28c22aa41548986dd6da476d877
DenisKent <kentdenis6@hotmail.co.uk>
2018-07-25 17:46:31 +0300

    Merged dom changes

--- public/dom.js
+++ public/dom.js
@@ -32,12 +32,14 @@ const arrOfCities = [
 var appendCities = function(arrOfCities) {
   var ul = document.getElementById("dropdown");
   ul.innerHTML = "";
-<<<<<<< HEAD
   if (searchInput.value.length > 0) {
     arrOfCities.forEach(function(city) {
       let li = document.createElement("li");
       let divCity = document.createElement("div");
       let divCountry = document.createElement("div");
+      li.classList.add("liList");
+      divCity.classList.add("cityName");
+      divCountry.classList.add("countryName");
       divCity.textContent = city.name;
       divCountry.textContent = city.country;
       ul.appendChild(li);
@@ -60,36 +62,4 @@ var appendCities = function(arrOfCities) {
       );
     });
   }
-=======
-  arrOfCities.forEach(function(city) {
-    let li = document.createElement("li");
-    let divCity = document.createElement("div");
-    let divCountry = document.createElement("div");
-    li.classList.add("liList");
-    divCity.classList.add("cityName");
-    divCountry.classList.add("countryName");
-    divCity.textContent = city.name;
-    divCountry.textContent = "(" + city.country + ")";
-    ul.appendChild(li);
-    li.appendChild(divCity);
-    li.appendChild(divCountry);
-    li.addEventListener(
-      "click",
-      function(e) {
-        var cityForAPI = e.target.firstChild.textContent;
-        var query = city.name;
-        var id = "pxlrimage";
-        pixabyXhrApi(query, id); //Needs console
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox.streets',
-          accessToken: 'pk.eyJ1IjoiZGVuaXNrZW50IiwiYSI6ImNqazEyN25tYjBjM3EzdGtjZXM5Mnp3dXMifQ.MluPs6OSZ5OPL-QU_61Low'
-      }).addTo(mymap);
-      },
-      false
-    );
-  });
->>>>>>> master
 };
