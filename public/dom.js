69a4941ee0f1f4cdbaf8ec2d9439716b6a2293ce
DenisKent <kentdenis6@hotmail.co.uk>
2018-07-25 15:03:43 +0300

    Maps API

--- public/dom.js
+++ public/dom.js
@@ -4,7 +4,14 @@ console.log(searchInput);
 var sendForm = function () {
   var url = searchWrapper(searchInput.value);
 
-  xhrAPI(url, appendCities);
+  xhrAPI(url, console.log);
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZGVuaXNrZW50IiwiYSI6ImNqazEyN25tYjBjM3EzdGtjZXM5Mnp3dXMifQ.MluPs6OSZ5OPL-QU_61Low'
+}).addTo(mymap);
 };
 
 //creates a drop down menu from the array of cities. Is called by an XHR request.
--- src/citySearch.js
+++ src/citySearch.js
@@ -1,12 +1,23 @@
 const citiesJSON = require("./cities.json");
+const countryNamesJSON = require("./countryNames.json");
 
 const search = function(input, cities = citiesJSON) {
-
-  var allMatches = includesSearch(input, cities);
-  var sortedMatches = sortByNameLength(allMatches);
-  var top8Matches = sortedMatches.slice(0,8);
-  return top8Matches;
+  var allMatches = includesSearch(input, cities); //Finds any matches that contain the input
+  var completeMatches = allMatches.filter(city =>  "country" in city); //Excludes all cities that don't have a country property
+  var sortedMatches = sortByNameLength(completeMatches); //Sorts matches so that shorter matches are pushed to the front. This means that user typed cities will be prefered
+  var top8Matches = sortedMatches.slice(0,8); //Cuts off all but top 8 cities
+  var top8Formated = countryCodeToName(top8Matches); //Changes the country name from 2 letter code to full name
+  return top8Formated;
 }
+
+const countryCodeToName = function(arr,countryNames = countryNamesJSON) {
+  return arr.map(function(city){
+    if (city.country in countryNames){
+      city.country = countryNames[city.country];
+    }
+    return city;
+  })
+};
 
 const includesSearch = function (input, cities){
   let output = [];
@@ -29,5 +40,6 @@ module.exports = {
   search,
   includesSearch,
   sortByNameLength,
-  citiesJSON
+  citiesJSON,
+  countryCodeToName
 }
